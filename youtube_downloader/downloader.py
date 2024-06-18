4a7b11d6243ef6b4083b1727c7e23b9ef8a9b7ee
Isaac Wines <isaacjwines@yahoo.com>
2024-06-17 21:26:34 -0400

    Downloader With click finished and running

--- youtube_downloader/downloader.py
+++ youtube_downloader/downloader.py
@@ -20,14 +20,14 @@ class Downloader:
         try: 
             yt = pt.YouTube(self.link) 
         except: 
-            print("Connection Error")
+            exit("Connection Error")
 
         if self.choice == "mp4":
             
             try: 
                 yt.streams.get_audio_only().download(filename=os.path.join(self.path, f'{self.name}.mp4'))
             except: 
-                print("Unable to Download")
+                exit("Unable to Download")
 
         if self.choice == "mp3":
 
@@ -45,15 +45,29 @@ class Downloader:
             os.remove(os.path.join(self.path, f'{self.name}.mp4'))
 
 @click.command()
-@click.option("--choice", "-c", is_flag = True, help="Enter either 'mp3' or 'mp4' to decide file format.")
-@click.option("--link", "-l", multiple = True, help='Enter the youtube link.')
-@click.option("--output", "-o", multiple = True, help='Enter the desired file name.')
-@click.option("--path", "-p", multiple = True, help='Enter the desired file output path, leave blank to place video in default downloads folder.')           
+@click.option("--choice", "-c", required = True, type = str, help="Enter either 'mp3' or 'mp4' to decide file format. Choice will be made for every link entered.")
+@click.option("--link", "-l", multiple = True, required = True, help='Enter the youtube link. If you wish to download more than one format as such: "-o output1 -l link1 -o output2 -l link2" ')
+@click.option("--output", "-o", multiple = True, required = True, type = str, help='Enter the desired file name. If there are multipe links format as such: "-o output1 -l link1 -o output2 -l link2"')
+@click.option("--path", "-p", type = str, help='Enter the desired file output path, leave blank to place video in default downloads folder.')           
 def main(choice,link,output,path):
+    
+    
+    if choice not in ["mp3","mp4"]:
+        exit("Invalid File Name")
 
-    song = Downloader("https://youtu.be/atgjKEgSqSU?si=_OrwASXqEg5g6_Bk", ".\\downloads", "mp4", "ariamath")
-    song.folders()
-    song.download()
+    if not output:
+        exit("Filename is Required.")
+
+    if not path:
+        path = "..\\downloads"
+
+    if len(output) != len(link):
+        exit("Number of designated outputs do not much number of entered links.")
+
+    for x in range(len(link)):
+        song = Downloader(link[x], path, choice, output[x])
+        song.folders()
+        song.download()
 
 if __name__ == "__main__":
     main()
